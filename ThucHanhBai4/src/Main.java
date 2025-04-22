8cc039480a8d8ca656733e16dde82fc081aeacc1
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-22 14:25:07 +0700

    add

---
+++ ThucHanhBai4/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //create a BST
+        BST<String> tree = new BST<>();
+        tree.insert("George");
+        tree.insert("Michael");
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Jones");
+        tree.insert("Peter");
+        tree.insert("Daniel");
+        //traverse tree
+        System.out.println("Inorder (sorted): ");
+        tree.inorder();
+        System.out.println("The number of nodes is: " + tree.getSize());
+    }
+}
